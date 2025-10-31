fc43b46c8071941a1e92d56b508588f99be8b0bb
Matsu033 <matsutai15@icloud.com>
2025-10-31 16:49:06 +0900

    画面の確認お願いします。

--- app/src/main/java/com/example/word_policing/MainActivity.java
+++ app/src/main/java/com/example/word_policing/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.word_policing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ruleButton = (Button) findViewById(R.id.rule);
+        ruleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
