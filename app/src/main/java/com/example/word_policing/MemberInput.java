bc03ae5ff177b4165aeffe3e6bfa56c232bb8acd
SatsukiOyama <kousatsuki10090223@icloud.com>
2025-10-31 15:29:53 +0900

    MemberInput.java作成したよ

---
+++ app/src/main/java/com/example/word_policing/MemberInput.java
@@ -0,0 +1,14 @@
+package com.example.word_policing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_input);
+    }
+}
