4640e4ec81149d0752cb14741ab6570ac4dbd664
SatsukiOyama <kousatsuki10090223@icloud.com>
2025-10-31 16:48:22 +0900

    人数の増減のボタンと人数の表示を追加

--- app/src/main/java/com/example/word_policing/MemberInput.java
+++ app/src/main/java/com/example/word_policing/MemberInput.java
@@ -3,6 +3,9 @@ package com.example.word_policing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MemberInput extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class MemberInput extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_input);
+
+        Button leftbutton = (Button) findViewById(R.id.left);
+        leftbutton.setText(R.string.direction_left);
+        Button rightbutton = (Button) findViewById(R.id.right);
+        leftbutton.setText(R.string.direction_right);
+        TextView numtext = (TextView) findViewById(R.id.num);
+        numtext.setText(String.valueOf(2));   // 初期値
+
+        // TextViewから文字列を取得
+        String currentText = numtext.getText().toString();
+        // 文字列から数字を取り出す（整数に変換）
+        int currentNumber = Integer.parseInt(currentText);
+        leftbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 数字を加算または減算する
+                int newNumber = currentNumber + 1;  // +1
+
+                // 結果をTextViewにセット
+                numtext.setText(String.valueOf(newNumber));
+            }
+        });
+        rightbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 数字を加算または減算する
+                int newNumber = currentNumber - 1;  // -1
+
+                // 結果をTextViewにセット
+                numtext.setText(String.valueOf(newNumber));
+            }
+        });
+
     }
 }
