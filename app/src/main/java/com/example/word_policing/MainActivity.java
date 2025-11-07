e184e8a991e1b76ba9abf3cf0eb4444b1f7c5d70
kikitataki <seizei03010@gmail.com>
2025-11-07 14:03:33 +0900

    commit

--- app/src/main/java/com/example/word_policing/MainActivity.java
+++ app/src/main/java/com/example/word_policing/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.word_policing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button ruleButton = (Button) findViewById(R.id.rule);
-        ruleButton.setOnClickListener(new View.OnClickListener() {
+        // tmpボタンを取得
+        Button tmpButton = findViewById(R.id.tmp);
+
+        // クリック時に ThemeDisplay に遷移
+        tmpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThemeDisplay.class);
+                startActivity(intent);
             }
         });
     }
