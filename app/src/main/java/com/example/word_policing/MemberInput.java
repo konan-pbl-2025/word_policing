5d17912acfd582cac90498b602c6c2ca864b8359
SatsukiOyama <kousatsuki10090223@icloud.com>
2025-11-07 18:53:41 +0900

    なんも意味ないよ

--- app/src/main/java/com/example/word_policing/MemberInput.java
+++ app/src/main/java/com/example/word_policing/MemberInput.java
@@ -21,6 +21,7 @@ public class MemberInput extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_input);
 
+        // id取得
         Button leftbutton = (Button) findViewById(R.id.left);
         Button rightbutton = (Button) findViewById(R.id.right);
         Button addbutoon = (Button) findViewById(R.id.AddButton);
